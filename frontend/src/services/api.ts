60735c659db0613e26324363423c72ce31875003
MaoDehka <maodan.bride@gmail.com>
2025-06-25 23:08:16 +0200

    Déploiement DEV

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -13,7 +13,7 @@ function getBaseUrl() {
   console.log(`Plateforme détectée: ${isNative ? 'Native' : 'Web'}`);
   
   if (isNative) {
-    return 'http://192.168.1.154:8000/api';
+    return 'http://10.176.130.248:8000/api';
   } else {
     return 'http://localhost:8000/api';
   }
